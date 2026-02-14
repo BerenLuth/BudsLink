8f333465a5cd05f89c5eac30b53f40054c2a8933
maniacx <macs18max@gmail.com>
2026-02-13 23:40:26 -0700

    translation: We are not using script anymore

--- update-translation.sh
+++ update-translation.sh
@@ -2,11 +2,9 @@
 set -e
 
 APP_ID="io.github.maniacx.BudsLink"
-SCRIPT_APP_ID="io.github.maniacx.BudsLink.script"
 
 ROOT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 PO_DIR="$ROOT_DIR/po"
-LOCALE_DIR="$ROOT_DIR/locale"
 POT_FILE="$PO_DIR/${APP_ID}.pot"
 
 cd "$ROOT_DIR"
@@ -37,25 +35,6 @@ for file in "$PO_DIR"/*.po; do
     echo "Updating $lang"
 
     msgmerge --backup=off --update --no-fuzzy-matching "$file" "$POT_FILE"
-
-    if grep --silent "#, fuzzy" "$file"; then
-        fuzzy+=("$lang")
-    fi
 done
 
-for file in "$PO_DIR"/*.po; do
-    lang=$(basename "$file" .po)
-
-    target_dir="$LOCALE_DIR/$lang/LC_MESSAGES"
-    target_mo="$target_dir/$SCRIPT_APP_ID.mo"
-
-    mkdir -p "$target_dir"
-    msgfmt "$file" -o "$target_mo"
-
-    echo "Compiled $target_mo"
-done
-
-if [[ -v fuzzy ]]; then
-    echo "WARNING: Translations have fuzzy strings: ${fuzzy[*]}"
-fi
 
